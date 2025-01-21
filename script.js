f4e4947675ea25ef67b28bc3b4399cd1349a06a4
bahissshta <grewalbahishta@gmail.com>
2025-01-20 23:22:59 -0700

    all good :)

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('t_button').onclick = function() {
+    alert('Tokyo has over 2000 ramen shops!')
+}
+
+document.getElementById('h_button').onclick = function() {
+    alert('The signature dish of Hiroshima is Okonomiyaki')
+}
+
+document.getElementById('o_button').onclick = function() {
+    alert('Osaka is known as the kitchen of Japan :)')
+}
